a6cefda35a52f514dda90e7897d89f67af407007
Kamil <Kam@wp.pl>
2017-03-05 23:17:35 +0100

    First commit projekt

---
+++ FSC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FSC.MvcApplication" Language="C#" %>
---
+++ FSC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FSC.Startup))]
+namespace FSC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
