ba0bf10c330b5933eba5e5ca4dd95e8196f8a5a0
Kamil <stepien.kamil@gmail.com>
2017-03-26 20:00:59 +0200

    Add api CheckListController and Angular service checklists.service

---
+++ FSC/Controllers/api/CheckListController.cs
@@ -0,0 +1,107 @@
+﻿using AutoMapper;
+using FSC.DataLayer;
+using FSC.ViewModels.Api;
+using Microsoft.AspNet.Identity;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Http;
+
+namespace FSC.Controllers.api
+{
+    public class CheckListController : ApiController
+    {
+        private ApplicationDbContext applicationDB = null;
+
+        public CheckListController()
+        {
+            applicationDB = new ApplicationDbContext();
+        }
+
+        [HttpGet]
+        //[Route("api/CheckList/Get")]
+        public IEnumerable<CheckList> Get()
+        {
+            var userId = User.Identity.GetUserId();
+            var data = applicationDB.CheckLists
+                .Where(x => x.ParentId == 0)
+                .Where(x => x.UserId == userId)
+                .ToList();
+
+            return data;
+        }
+
+        [Route("api/CheckList/Get/{id:int:max(10000)}")]
+        public IHttpActionResult Get(int id)
+        {
+            if (id == 0)
+                return BadRequest();
+            var userId = User.Identity.GetUserId();
+            var checkList = applicationDB.CheckLists.Where(x => x.UserId == userId).First(x => x.Id == id);
+            if (checkList == null)
+                return NotFound();
+
+            var result = Mapper.Map<CheckListDisplayVM>(checkList);
+            var checkListItems = applicationDB.CheckLists.Where(x => x.ParentId == checkList.Id).ToList();
+            result.Items = Mapper.Map<List<CheckListItem>>(checkListItems);
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public IHttpActionResult Create([FromBody]CreateChecklistVM body)
+        {
+            if (string.IsNullOrWhiteSpace(body.Description))
+                return BadRequest();
+            var newChecklist = new CheckList();
+            newChecklist.Description = body.Description;
+            newChecklist.UserId = User.Identity.GetUserId();
+            applicationDB.CheckLists.Add(newChecklist);
+            applicationDB.SaveChanges();
+            body.Id = newChecklist.Id;
+
+            return Ok(body);
+        }
+
+        [HttpPut]
+        public IHttpActionResult Put(int id, [FromBody]CheckListDisplayVM value)
+        {
+            if (id == 0)
+                return BadRequest();
+
+            var checkList = Mapper.Map<CheckList>(value);
+            var checkListItems = Mapper.Map<List<CheckList>>(value.Items);
+
+            checkListItems.ForEach(x => x.ParentId = checkList.Id);
+            checkListItems.Add(checkList);
+            checkListItems.ForEach(x => x.UserId = User.Identity.GetUserId());
+            checkListItems.ForEach(x =>
+            {
+                if (x.Id == 0)
+                    applicationDB.Entry(x).State = EntityState.Added;
+                else
+                    applicationDB.Entry(x).State = EntityState.Modified;
+            });
+            applicationDB.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Delete(int id)
+        {
+            if (id == 0)
+                return BadRequest();
+            var checklist = applicationDB.CheckLists.First(x => x.Id == id);
+            if (checklist == null)
+                return NotFound();
+
+            applicationDB.CheckLists.Remove(checklist);
+            var checklistItems = applicationDB.CheckLists.Where(x => x.ParentId == id);
+            applicationDB.CheckLists.RemoveRange(checklistItems);
+            applicationDB.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ FSC/tsScripts/checklist/checklists.service.ts
@@ -0,0 +1,64 @@
+﻿import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http'
+import { Observable } from 'rxjs/Rx';
+
+
+export class Checklist {
+    constructor(public Id: number, public Description: string, public IsCompleted: boolean) {
+    }
+}
+
+export class CheckListDisplayVM {
+    constructor(public Id: number, public Description: string, public Items: Checklist[]) {
+    }
+}
+
+@Injectable()
+export class ChecklistsService {
+    constructor(private _http: Http) {
+
+    }
+
+    getCheckLists() {
+        return this._http.get(this.address + 'Get', this.requestOptions())
+            .map((response: Response) => <Checklist[]>response.json())
+            .catch(this.hendleError);
+    }
+
+    getCheckList(id: number) {
+        return this._http.get(this.address + 'Get/' + id)
+            .map((response: Response) => <CheckListDisplayVM>response.json())
+            .catch(this.hendleError);
+    }
+
+    deleteChecklist(Id: number) {
+        return this._http.delete(this.address  + Id)
+            .map((response: Response) => response.json())
+            .catch(this.hendleError);
+    }
+
+    createChecklist(list: Checklist) {
+        let body = JSON.stringify(list);
+        return this._http.post(this.address , body, this.requestOptions())
+            .map((response: Response) => <Checklist>response.json())
+            .catch(this.hendleError);
+    }
+
+    updateChecklist(list: CheckListDisplayVM) {
+        let body = JSON.stringify(list);
+        return this._http.put(this.address  + list.Id, body, this.requestOptions())
+            .map((response: Response) => response.json())
+            .catch(this.hendleError);
+    }
+
+    address: string = "api/CheckList/";
+    requestOptions(): RequestOptions {
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        return new RequestOptions({ headers: headers });
+    }
+    hendleError(error: Response) {
+        console.log(error);
+        return Observable.throw(error.json().error || 'Błąd pobierania!');
+    }
+
+}
