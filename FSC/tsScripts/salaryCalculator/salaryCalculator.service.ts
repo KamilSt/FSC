6ddbf5fdb66e23b75422bbf81babbc556be5908a
Kamil <stepien.kamil@gmail.com>
2017-07-03 12:52:38 +0200

    Salary Calculator modul Fron end

---
+++ FSC/tsScripts/salaryCalculator/salaryCalculator.service.ts
@@ -0,0 +1,27 @@
+﻿import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import { Order } from "../orders/order.model";
+import { salaryCalculatorVM, salaryCalculatorResult, SalaryCost } from "../salaryCalculator/salaryCalculator.model";
+
+@Injectable()
+export class SalaryService {
+    constructor(private _http: Http) { }
+
+    calculate(vm: salaryCalculatorVM) {
+        let body = JSON.stringify(vm);
+        return this._http.post(this.address, body, this.requestOptions())
+            .map((response: Response) => <salaryCalculatorResult>response.json())
+            .catch(this.hendleError);
+    }
+
+    address: string = "api/finances/salaryCalculator/";
+    requestOptions(): RequestOptions {
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        return new RequestOptions({ headers: headers });
+    }
+    hendleError(error: Response) {
+        console.log(error);
+        return Observable.throw(error.json().error || 'Błąd pobierania!');
+    }
+}
