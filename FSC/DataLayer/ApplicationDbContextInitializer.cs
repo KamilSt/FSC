8e142a947b643fe34cb8af02c38086abc6bbdd2d
Kamil <stepien.kamil@gmail.com>
2018-07-18 16:08:04 +0200

    add seed data to invoice template

---
+++ FSC/DataLayer/ApplicationDbContextInitializer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FSC.DataLayer
+{
+    public class ApplicationDbContextInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<ApplicationDbContext>
+    {
+        protected override void Seed(ApplicationDbContext context)
+        {
+            var invoiceTempl = new List<InvoiceTemplate>
+            {
+             new InvoiceTemplate { Id = 1, Template = "yyyy/mm/nr" , DocumentType="Invoice"}
+            };
+            invoiceTempl.ForEach(templ => context.InvoiceTemplates.Add(templ));
+            context.SaveChanges();
+        }
+    }
+}
