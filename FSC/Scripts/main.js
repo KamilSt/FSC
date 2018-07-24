a4c9adaccdf58d5b646e62099fd40bee53d49925
Kamil <stepien.kamil@gmail.com>
2018-07-24 10:48:03 +0200

    missing file

--- FSC/Scripts/main.js
+++ FSC/Scripts/main.js
@@ -5,3 +5,4 @@ var app_module_1 = require("./app.module");
 var platform = platform_browser_dynamic_1.platformBrowserDynamic();
 platform.bootstrapModule(app_module_1.AppModule);
 platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
+//# sourceMappingURL=main.js.map
