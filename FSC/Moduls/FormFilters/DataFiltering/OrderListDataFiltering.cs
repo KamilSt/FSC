ee54fb4ea8dbe4fe8b8552e5f552bf0b1882b01c
Kamil <stepien.kamil@gmail.com>
2018-12-15 15:26:53 +0100

    Creation Data Filtering Factory

---
+++ FSC/Moduls/FormFilters/DataFiltering/OrderListDataFiltering.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+using FSC.DataLayer;
+
+namespace FSC.Moduls.FormFilters.DataFiltering
+{
+    public class OrderListDataFiltering
+    {
+        public static IQueryable<Order> Filter<T>(IQueryable<Order> orders, string filters)
+        {
+            var ser = new JavaScriptSerializer();
+            var reqFilters = ser.Deserialize<List<FilterRequest>>(filters);
+            foreach (var filter in reqFilters)
+            {
+                switch (filter.key)
+                {
+                    case "NumberInvoice":
+                        if (!string.IsNullOrWhiteSpace(filter.value))
+                            orders = orders.Where(x => x.InvoiceDocuments.Any(y => y.InvoiceNmuber.Contains(filter.value)));
+                        break;
+
+                    case "CompanyName":
+                        if (!string.IsNullOrWhiteSpace(filter.value))
+                            orders = orders.Where(x => x.Customer.CompanyName.Contains(filter.value));
+                        break;
+
+                    case "DocumentType":
+                        if (!string.IsNullOrEmpty(filter.value))
+                        {
+                            if (filter.value.Equals("Invoice"))
+                                orders = orders.Where(x => x.Invoiced == true);
+                            else if (filter.value.Equals("Correction"))
+                                orders = orders.Where(x => x.Description.Contains("Corection Invoice"));
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+            return orders;
+        }
+    }
+}
---
+++ FSC/Moduls/FormFilters/DataFilteringFactory.cs
@@ -0,0 +1,26 @@
+﻿using FSC.DataLayer;
+using FSC.Moduls.FormFilters.DataFiltering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FSC.Moduls.FormFilters
+{
+    public static class DataFilteringFactory
+    {
+        public const string FilterFor_OrderListFilter = "OrderListFilter";
+
+        public static IQueryable<T> GetFor<T>(string filterName, IQueryable<T> list, string filters)
+        {
+            switch (filterName)
+            {
+                case FilterFor_OrderListFilter:
+                    {
+                        return OrderListDataFiltering.Filter<Order>(list.Cast<Order>(), filters).Cast<T>();
+                    }
+            }
+            return null;
+        }
+    }
+}
